e16df3fd4ce2cbdc418088229bc2047c18dc23ca
YSEtheCODER <69650794+YSEtheCODER@users.noreply.github.com>
2020-10-14 21:40:07 +0300

    Add files via upload

---
+++ fina puan hesaplayıcı.py
@@ -0,0 +1,300 @@
+ad=input("İSİM GİRİN :")
+soyad=input("SOYAD GİRİN:")
+dereceler=float(input("DERECENİZİ GİRİN:"))
+brans=input("BRANŞINIZI GİRİN:")
+cinsiyet=input("CİNSİYETİNİZ: ")
+fullisim=ad+" "+soyad
+
+
+
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 663")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 589")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 526")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 471")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 424")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 383")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 347")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+" "+"FİNA POİNT 316")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 288")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 263")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 241")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 222")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 204")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 188")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 174")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="kelebek")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 161")
+    
+    
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 532")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 473")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 422")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 378")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 340")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 308")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 279")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 253")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 231")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 211")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 193")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 178")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 164")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 151")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 140")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="serbest")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 129")
+    
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 702")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 624")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 557")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 499")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 449")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 406")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 368")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 334")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 305")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 279")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 255")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 235")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 216")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 199")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 184")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="sırt")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 171")
+    
+
+
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 771")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 685")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 612")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 549")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 494")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 446")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 404")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 367")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 335")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 306")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 281")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 258")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 238")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 219")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 203")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="kurbağa")and(cinsiyet=="erkek"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 188") 
+    
+    
+    
+    
+    
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 927")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 824")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 736")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 660")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 594")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 536")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 486")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+" "+"FİNA POİNT 442")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 403")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 368")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 337")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 310")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 286")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 264")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 244")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="kelebek")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 226")
+    
+    
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 771")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 685")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 612")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 549")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 494")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 446")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 404")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 367")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 335")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 306")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 281")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 258")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 238")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 219")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 203")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="serbest")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 188")
+    
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 1082")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 962")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 859")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 770")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 693")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 626")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 567")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 516")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 470")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 430")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 394")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 362")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 333")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 308")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 285")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="sırt")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 264")
+    
+
+
+    
+    
+if (dereceler>=25.0)and(dereceler<26.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 1503")
+if (dereceler>=26.0)and(dereceler<27.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 1336")
+if (dereceler>=27.0)and(dereceler<28.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 1193")
+if (dereceler>=28.0)and(dereceler<29.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 1070")
+if (dereceler>=29.0)and(dereceler<30.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 963")
+if (dereceler>=30.0)and(dereceler<31.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 870")
+if (dereceler>=31.0)and(dereceler<32.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 788")
+if (dereceler>=32.0)and(dereceler<33.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 716")
+if (dereceler>=33.0)and(dereceler<34.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 653")
+if (dereceler>=34.0)and(dereceler<35.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 597")
+if (dereceler>=35.0)and(dereceler<36.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 547")
+if (dereceler>=36.0)and(dereceler<37.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 503")
+if (dereceler>=37.0)and(dereceler<38.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 463")
+if (dereceler>=38.0)and(dereceler<39.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 428")
+if (dereceler>=39.0)and(dereceler<40.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 396")
+if (dereceler>=40.0)and(dereceler<41.0)and(brans=="kurbağa")and(cinsiyet=="kız"):
+    print(fullisim+" "+brans+" "+"FİNA POİNT 367")
+    
+    
+    
+print("İYİ YARIŞLAR!!!!")
